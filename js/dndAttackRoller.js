bc9df40697b52bb2b0ade2f0ab424ba36595eaf9
StygSoftworks <108203434+StygSoftworks@users.noreply.github.com>
2023-06-13 01:42:54 -0400

    Update dndAttackRoller.js

--- js/dndAttackRoller.js
+++ js/dndAttackRoller.js
@@ -1,195 +1,197 @@
 $(document).ready(function() {
-    $('#savingThrowsTable').DataTable();
-    $('#weaponsTable').DataTable();
+  $('#savingThrowsTable').DataTable();
+  $('#weaponsTable').DataTable();
+});
+
+function rollDice(button) {
+  var row = $(button).closest('tr');
+  var fort = row.find('#fort').val();
+  var reflex = row.find('#reflex').val();
+  var will = row.find('#will').val();
+
+  var fortRoll = Math.floor(Math.random() * 20) + 1 + parseInt(fort);
+  var reflexRoll = Math.floor(Math.random() * 20) + 1 + parseInt(reflex);
+  var willRoll = Math.floor(Math.random() * 20) + 1 + parseInt(will);
+
+  alert("Fort: " + fortRoll + "\nReflex: " + reflexRoll + "\nWill: " + willRoll);
+}
+
+function toggleModifiersPanel() {
+  var strengthMod = $('#strengthMod').val();
+  var dexMod = $('#dexMod').val();
+
+  if (strengthMod !== '' || dexMod !== '') {
+    $('#modifiersPanel').collapse('show');
+  } else {
+    $('#modifiersPanel').collapse('hide');
+  }
+}
+
+function rollAttack(button) {
+  var row = $(button).closest('tr');
+  var attackBonus = row.find('#attackBonus').val();
+  var damage = row.find('#damage').val();
+
+  // Evaluate damage expression
+  var evaluatedDamage = evaluateDamageExpression(damage);
+
+  var attackRoll = Math.floor(Math.random() * 20) + 1 + parseInt(attackBonus);
+
+  alert("Attack Roll: " + attackRoll + "\nDamage: " + evaluatedDamage);
+}
+
+function evaluateDamageExpression(expression) {
+  var diceRegex = /(\d+d\d+([\+\-]\d+)?)/g;
+  var matches = expression.match(diceRegex);
+  var result = '';
+
+  if (matches) {
+    for (var i = 0; i < matches.length; i++) {
+      var dice = matches[i];
+      var rollResult = rollDiceExpression(dice);
+      result += dice + ': ' + rollResult + ' ';
+    }
+  }
+
+  return result.trim();
+}
+
+function rollDiceExpression(diceExpression) {
+  var regex = /(\d+)d(\d+)([\+\-]\d+)?/;
+  var matches = diceExpression.match(regex);
+  var quantity = parseInt(matches[1]);
+  var sides = parseInt(matches[2]);
+  var modifier = matches[3] ? parseInt(matches[3]) : 0;
+  var total = 0;
+
+  for (var i = 0; i < quantity; i++) {
+    total += Math.floor(Math.random() * sides) + 1;
+  }
+
+  return total + modifier;
+}
+
+function addRow() {
+  var newRow = '<tr>' +
+    '<td><input type="text" class="form-control" id="weaponName"></td>' +
+    '<td><input type="number" class="form-control" id="attackBonus"></td>' +
+    '<td><input type="text" class="form-control" id="damage"></td>' +
+    '<td><input type="text" class="form-control" id="weaponType"></td>' +
+    '<td><input type="text" class="form-control" id="range"></td>' +
+    '<td>' +
+    '<button class="btn btn-primary" onclick="rollAttack(this)">Roll Attack</button>' +
+    '<button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>' +
+    '</td>' +
+    '</tr>';
+
+  $('#weaponsTable tbody').append(newRow);
+}
+
+function deleteRow(button) {
+  $(button).closest('tr').remove();
+}
+
+
+function saveFieldsToCookie() {
+  var fields = {};
+
+  // Save character info
+  fields.name = $('#name').val();
+  fields.baseAttackBonus = $('#baseAttackBonus').val();
+  fields.currentHP = $('#currentHP').val();
+  fields.maxHP = $('#maxHP').val();
+
+  // Save saving throws
+  fields.fort = $('#fort').val();
+  fields.reflex = $('#reflex').val();
+  fields.will = $('#will').val();
+
+  // Save modifiers
+  fields.strengthMod = $('#strengthMod').val();
+  fields.dexMod = $('#dexMod').val();
+
+  // Save weapons
+  fields.weapons = [];
+
+  $('#weaponsTable tbody tr').each(function() {
+    var weapon = {};
+    weapon.name = $(this).find('#weaponName').val();
+    weapon.attackBonus = $(this).find('#attackBonus').val();
+    weapon.damage = $(this).find('#damage').val();
+    weapon.weaponType = $(this).find('#weaponType').val();
+    weapon.range = $(this).find('#range').val();
+    fields.weapons.push(weapon);
   });
 
-  function rollDice(button) {
-    var row = $(button).closest('tr');
-    var fort = row.find('#fort').val();
-    var reflex = row.find('#reflex').val();
-    var will = row.find('#will').val();
+  // Save fields to cookie
+  var jsonFields = JSON.stringify(fields);
 
-    var fortRoll = Math.floor(Math.random() * 20) + 1 + parseInt(fort);
-    var reflexRoll = Math.floor(Math.random() * 20) + 1 + parseInt(reflex);
-    var willRoll = Math.floor(Math.random() * 20) + 1 + parseInt(will);
-
-    alert("Fort: " + fortRoll + "\nReflex: " + reflexRoll + "\nWill: " + willRoll);
-  }
-
-  function toggleModifiersPanel() {
-    var strengthMod = $('#strengthMod').val();
-    var dexMod = $('#dexMod').val();
-
-    if (strengthMod !== '' || dexMod !== '') {
-      $('#modifiersPanel').collapse('show');
-    } else {
-      $('#modifiersPanel').collapse('hide');
-    }
-  }
-
-  function rollAttack(button) {
-    var row = $(button).closest('tr');
-    var attackBonus = row.find('#attackBonus').val();
-    var damage = row.find('#damage').val();
-
-    // Evaluate damage expression
-    var evaluatedDamage = evaluateDamageExpression(damage);
-
-    var attackRoll = Math.floor(Math.random() * 20) + 1 + parseInt(attackBonus);
-
-    alert("Attack Roll: " + attackRoll + "\nDamage: " + evaluatedDamage);
-  }
-
-  function evaluateDamageExpression(expression) {
-    var diceRegex = /(\d+d\d+([\+\-]\d+)?)/g;
-    var matches = expression.match(diceRegex);
-    var result = '';
-
-    if (matches) {
-      for (var i = 0; i < matches.length; i++) {
-        var dice = matches[i];
-        var rollResult = rollDiceExpression(dice);
-        result += dice + ': ' + rollResult + ' ';
-      }
-    }
-
-    return result.trim();
-  }
-
-  function rollDiceExpression(diceExpression) {
-    var regex = /(\d+)d(\d+)([\+\-]\d+)?/;
-    var matches = diceExpression.match(regex);
-    var quantity = parseInt(matches[1]);
-    var sides = parseInt(matches[2]);
-    var modifier = matches[3] ? parseInt(matches[3]) : 0;
-    var total = 0;
-
-    for (var i = 0; i < quantity; i++) {
-      total += Math.floor(Math.random() * sides) + 1;
-    }
-
-    return total + modifier;
-  }
-
-  function addRow() {
-    var newRow = '<tr>' +
-      '<td><input type="text" class="form-control" id="weaponName"></td>' +
-      '<td><input type="number" class="form-control" id="attackBonus"></td>' +
-      '<td><input type="text" class="form-control" id="damage"></td>' +
-      '<td><input type="text" class="form-control" id="weaponType"></td>' +
-      '<td><input type="text" class="form-control" id="range"></td>' +
-      '<td>' +
-      '<button class="btn btn-primary" onclick="rollAttack(this)">Roll Attack</button>' +
-      '<button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>' +
-      '</td>' +
-      '</tr>';
-
-    $('#weaponsTable tbody').append(newRow);
-  }
-
-  function deleteRow(button) {
-    $(button).closest('tr').remove();
-  }
+  // URL-encode the fields JSON string
+  var encodedFieldsJson = encodeURIComponent(jsonFields);
 
 
-  function saveFieldsToCookie() {
-    var fields = {};
-  
-    // Save character info
-    fields.name = $('#name').val();
-    fields.baseAttackBonus = $('#baseAttackBonus').val();
-    fields.currentHP = $('#currentHP').val();
-    fields.maxHP = $('#maxHP').val();
-  
-    // Save saving throws
-    fields.fort = $('#fort').val();
-    fields.reflex = $('#reflex').val();
-    fields.will = $('#will').val();
-  
-    // Save modifiers
-    fields.strengthMod = $('#strengthMod').val();
-    fields.dexMod = $('#dexMod').val();
-  
-    // Save weapons
-    fields.weapons = [];
-  
-    $('#weaponsTable tbody tr').each(function() {
-      var weapon = {};
-      weapon.name = $(this).find('#weaponName').val();
-      weapon.attackBonus = $(this).find('#attackBonus').val();
-      weapon.damage = $(this).find('#damage').val();
-      weapon.weaponType = $(this).find('#weaponType').val();
-      weapon.range = $(this).find('#range').val();
-      fields.weapons.push(weapon);
-    });
-  
-    // Save fields to cookie
-    var jsonFields = JSON.stringify(fields);
-
-    // URL-encode the fields JSON string
-    var encodedFieldsJson = encodeURIComponent(jsonFields);
-
-
-    //document.cookie = 'characterFields=' + jsonFields;
-    document.cookie = 'characterFields=' + encodedFieldsJson;
-  }
+  //document.cookie = 'characterFields=' + jsonFields;
+  document.cookie = 'characterFields=' + encodedFieldsJson;
+}
 
 
 function loadFieldsFromCookie() {
-  // Get cookie value
-  //var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)characterFields\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+// Get cookie value
+//var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)characterFields\s*\=\s*([^;]*).*$)|^.*$/, '$1');
 
+console.log(document.cookie);
 
-
-  var cookieValue = document.cookie.split(';');
-  var characterFieldsCookie = null;
-  for (var i = 0; i < cookieValue.length; i++) {
-    var cookie = cookieValue[i].trim();
-    if (cookie.indexOf('characterFields=') === 0) {
-      characterFieldsCookie = cookie;
-      break;
-    }
+var cookieValue = document.cookie.split(';');
+var characterFieldsCookie = null;
+for (var i = 0; i < cookieValue.length; i++) {
+  var cookie = cookieValue[i].trim();
+  if (cookie.indexOf('characterFields=') === 0) {
+    characterFieldsCookie = cookie;
+    break;
   }
+}
 
-  var fieldsJson = decodeURIComponent(characterFieldsCookie.split('=')[1]);
 
-  var fields = JSON.parse(fieldsJson);
-  
-  // Parse cookie value
-  //var fields = JSON.parse(cookieValue);
 
-  // Load character info
-  $('#name').val(fields.name);
-  $('#baseAttackBonus').val(fields.baseAttackBonus);
-  $('#currentHP').val(fields.currentHP);
-  $('#maxHP').val(fields.maxHP);
+var fieldsJson = decodeURIComponent(characterFieldsCookie.split('=')[1]);
 
-  // Load saving throws
-  $('#fort').val(fields.fort);
-  $('#reflex').val(fields.reflex);
-  $('#will').val(fields.will);
+var fields = JSON.parse(fieldsJson);
 
-  // Load modifiers
-  $('#strengthMod').val(fields.strengthMod);
-  $('#dexMod').val(fields.dexMod);
+// Parse cookie value
+//var fields = JSON.parse(cookieValue);
 
-  // Load weapons
-  $('#weaponsTable tbody').empty();
+// Load character info
+$('#name').val(fields.name);
+$('#baseAttackBonus').val(fields.baseAttackBonus);
+$('#currentHP').val(fields.currentHP);
+$('#maxHP').val(fields.maxHP);
 
-  for (var i = 0; i < fields.weapons.length; i++) {
-    var weapon = fields.weapons[i];
-    var newRow = '<tr>' +
-      '<td><input type="text" class="form-control" id="weaponName" value="' + weapon.name + '"></td>' +
-      '<td><input type="number" class="form-control" id="attackBonus" value="' + weapon.attackBonus + '"></td>' +
-      '<td><input type="text" class="form-control" id="damage" value="' + weapon.damage + '"></td>' +
-      '<td><input type="text" class="form-control" id="weaponType" value="' + weapon.weaponType + '"></td>' +
-      '<td><input type="text" class="form-control" id="range" value="' + weapon.range + '"></td>' +
-      '<td>' +
-      '<button class="btn btn-primary" onclick="rollAttack(this)">Roll Attack</button>' +
-      '<button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>' +
-      '</td>' +
-      '</tr>';
+// Load saving throws
+$('#fort').val(fields.fort);
+$('#reflex').val(fields.reflex);
+$('#will').val(fields.will);
 
-    $('#weaponsTable tbody').append(newRow);
-  }
+// Load modifiers
+$('#strengthMod').val(fields.strengthMod);
+$('#dexMod').val(fields.dexMod);
+
+// Load weapons
+$('#weaponsTable tbody').empty();
+
+for (var i = 0; i < fields.weapons.length; i++) {
+  var weapon = fields.weapons[i];
+  var newRow = '<tr>' +
+    '<td><input type="text" class="form-control" id="weaponName" value="' + weapon.name + '"></td>' +
+    '<td><input type="number" class="form-control" id="attackBonus" value="' + weapon.attackBonus + '"></td>' +
+    '<td><input type="text" class="form-control" id="damage" value="' + weapon.damage + '"></td>' +
+    '<td><input type="text" class="form-control" id="weaponType" value="' + weapon.weaponType + '"></td>' +
+    '<td><input type="text" class="form-control" id="range" value="' + weapon.range + '"></td>' +
+    '<td>' +
+    '<button class="btn btn-primary" onclick="rollAttack(this)">Roll Attack</button>' +
+    '<button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>' +
+    '</td>' +
+    '</tr>';
+
+  $('#weaponsTable tbody').append(newRow);
+}
 }
